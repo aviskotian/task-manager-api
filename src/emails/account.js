c45cd3a79a279d177328fd14ae1e05a083c67964
Avinash Amin <avinash.amin@globaldelight.com>
2020-04-25 14:43:53 +0530

    Disbale Sendgrid

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,27 +1,27 @@
-const sgMail = require('@sendgrid/mail')
+// const sgMail = require('@sendgrid/mail')
 
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendWelcomeEmail = (email, name) => {
-    sgMail.send({
-        to: email,
-        from: 'avinashskotian@gmail.com',
-        subject: 'Thanks for Joining in!',
-        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
-    })
-}
+// const sendWelcomeEmail = (email, name) => {
+//     sgMail.send({
+//         to: email,
+//         from: 'avinashskotian@gmail.com',
+//         subject: 'Thanks for Joining in!',
+//         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+//     })
+// }
    
-const sendCancelEmail = (email, name) => {
-    sgMail.send({
-        to: email,
-        from: 'avinashskotian@gmail.com',
-        subject: 'Good Bye!',
-        text: `Hello, ${name}. Your account has been deleted successfully!`
-    })
-}
-module.exports = {
-    sendWelcomeEmail,
-    sendCancelEmail
-}
+// const sendCancelEmail = (email, name) => {
+//     sgMail.send({
+//         to: email,
+//         from: 'avinashskotian@gmail.com',
+//         subject: 'Good Bye!',
+//         text: `Hello, ${name}. Your account has been deleted successfully!`
+//     })
+// }
+// module.exports = {
+//     sendWelcomeEmail,
+//     sendCancelEmail
+// }
 
